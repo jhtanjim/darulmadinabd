87a654397908c670cc527dc682ac96ac5a4e317c
jhtanjim <jh.tanjim.4@gmail.com>
2025-10-06 22:43:43 -0700

    make some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,20 @@ import About from "@/components/Home/About/About";
 import Banner from "@/components/Home/Banner/Banner";
 import CampusLocation from "@/components/Home/CampusLocation/CampusLocation";
 import CurriculumSection from "@/components/Home/Curriculum/Curriculum";
-import PdfBookImages from "@/components/Home/PdfBookImages/PdfBookImages";
 import Review from "@/components/Home/Review/Review";
 import SpecialMoment from "@/components/Home/SpecialMoment/SpecialMoment";
 import WorldCampus from "@/components/Home/worldCampus/worldCampus";
+import dynamic from "next/dynamic";
+
+// ✅ Dynamically import PdfBookImages with SSR disabled
+const PdfBookImages = dynamic(
+  () => import("@/components/Home/PdfBookImages/PdfBookImages"),
+  { ssr: false }
+);
 
 const Home = () => {
   return (
-    <div className="text-3xl  font-bold align-content-center pt-[10vh]">
+    <div className="text-3xl font-bold align-content-center pt-[10vh]">
       <Banner />
       <About />
       <CurriculumSection />
