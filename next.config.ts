bac884000c8c4faf723fa03e0023fd315b529521
jhtanjim <jh.tanjim.4@gmail.com>
2025-10-06 23:05:47 -0700

    checkpdf

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "images.pexels.com","encrypted-tbn0.gstatic.com","www.darulmadinah.net",'images.unsplash.com',"darulmadinah.edu.in","pbs.twimg.com","https://img.freepik.com"],
+    domains: [
+      "images.unsplash.com",
+      "images.pexels.com",
+      "encrypted-tbn0.gstatic.com",
+      "www.darulmadinah.net",
+      "darulmadinah.edu.in",
+      "pbs.twimg.com",
+      "img.freepik.com",
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+        encoding: false,
+      };
+    }
+    return config;
   },
 };
 
